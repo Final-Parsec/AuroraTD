8c5fe3d6b443f21a5c0edca6b28038f89bc58019
Matt <matthewbauer21@gmail.com>
2015-09-19 13:56:28 -0500

    added back screenanimationevents

---
+++ Assets/Scripts/GUI/ScreenAnimationEvents.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenAnimationEvents : MonoBehaviour {
+	
+	
+	public void FadeOutFinished()
+	{
+		gameObject.SetActive (false);
+	}
+}
